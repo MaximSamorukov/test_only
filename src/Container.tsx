ba000e4577ce2fec77f2cad6f5f881bb146b19eb
maksim.samorukov <maxim.samorukov@gmail.com>
2025-12-07 17:06:31 +0300

    feat: Enhance dot hover animations by replacing single circles with nested SVG elements and introducing new radius and background color constants.

--- src/Container.tsx
+++ src/Container.tsx
@@ -6,13 +6,15 @@ import { ItemsSlider } from './components/ItemsSlider';
 import { calculateDots, getDelta, getRealIndex } from './utils';
 import {
   CIRCLE_RADIUS,
-  DOT_RADIUS,
+  START_DOT_RADIUS,
+  END_DOT_RADIUS,
   FULL_CIRCLE_DEG,
   MAX_HEIGHT,
   MAX_WIDTH,
   PATH_COLOR,
   X_CENTER,
   Y_CENTER,
+  BACKGROUND_COLOR,
 } from './constants';
 import { historicalDataStore } from './store';
 import { observer } from 'mobx-react-lite';
@@ -47,21 +49,66 @@ export const Container = observer(() => {
   };
 
   const handleDotMouseEnter =
-    (dotId: number) => (event: React.MouseEvent<SVGCircleElement>) => {
+    (dotId: number) => (event: React.MouseEvent<SVGGElement>) => {
       const realIndex = getRealIndex(currentIndex, dotId, periods.length);
-      gsap.to(event.currentTarget, {
-        attr: { r: 14 },
-        duration: 0.3,
-        ease: 'power2.out',
-      });
+      const outerCircle = event.currentTarget.querySelector('#outer_circle');
+      const innerCircle = event.currentTarget.querySelector('#inner_circle');
+      const text = event.currentTarget.querySelector('text');
+
+      gsap
+        .timeline()
+        .to(outerCircle, {
+          attr: { r: END_DOT_RADIUS },
+          fill: PATH_COLOR,
+          duration: 0.2,
+          ease: 'power2.out',
+        })
+        .to(innerCircle, {
+          attr: { r: END_DOT_RADIUS - 0.6 },
+          fill: BACKGROUND_COLOR,
+          duration: 0.2,
+          ease: 'power2.out',
+        })
+        .to(
+          text,
+          {
+            opacity: 1,
+            duration: 0.4,
+            ease: 'power2.out',
+          },
+          '-=0.1',
+        );
     };
 
-  const handleDotMouseLeave = (event: React.MouseEvent<SVGCircleElement>) => {
-    gsap.to(event.currentTarget, {
-      attr: { r: DOT_RADIUS },
-      duration: 0.3,
-      ease: 'power2.out',
-    });
+  const handleDotMouseLeave = (event: React.MouseEvent<SVGGElement>) => {
+    const outerCircle = event.currentTarget.querySelector('#outer_circle');
+    const innerCircle = event.currentTarget.querySelector('#inner_circle');
+
+    const text = event.currentTarget.querySelector('text');
+
+    gsap
+      .timeline()
+      .to(text, {
+        opacity: 0,
+        duration: 0.1,
+        ease: 'power2.out',
+      })
+      .to(innerCircle, {
+        attr: { r: 0 },
+        fill: PATH_COLOR,
+        stroke: 'none',
+        strokeWidth: 0,
+        duration: 0.1,
+        ease: 'power2.out',
+      })
+      .to(outerCircle, {
+        attr: { r: START_DOT_RADIUS },
+        fill: PATH_COLOR,
+        stroke: 'none',
+        strokeWidth: 0,
+        duration: 0.2,
+        ease: 'power2.out',
+      });
   };
 
   return (
@@ -76,17 +123,46 @@ export const Container = observer(() => {
         />
         <g ref={groupRef}>
           {dots.map((dot) => (
-            <circle
+            <g
               key={dot.id}
-              cx={dot.x}
-              cy={dot.y}
-              r={DOT_RADIUS}
-              fill="black"
               onMouseEnter={handleDotMouseEnter(dot.id)}
               onMouseLeave={handleDotMouseLeave}
               className={`${s.dot} + ' ' + ${dot.id}`}
               onClick={() => handleDotClick(dot.id)}
-            />
+              style={{ cursor: 'pointer' }}
+            >
+              <circle
+                id="outer_circle"
+                cx={dot.x}
+                cy={dot.y}
+                r={START_DOT_RADIUS}
+                fill="black"
+              />
+              <circle
+                id="inner_circle"
+                cx={dot.x}
+                cy={dot.y}
+                r={0}
+                fill="black"
+              />
+              <text
+                x={dot.x}
+                y={dot.y}
+                textAnchor="middle"
+                dominantBaseline="central"
+                fontSize="14"
+                fill="black"
+                opacity={0}
+                style={{
+                  pointerEvents: 'none',
+                  userSelect: 'none',
+                  fontSize: '20px',
+                  color: PATH_COLOR,
+                }}
+              >
+                {dot.id}
+              </text>
+            </g>
           ))}
         </g>
       </svg>
